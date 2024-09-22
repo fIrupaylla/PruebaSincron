627a19506fcd20e676307ae10544905ccd86eb19
Fiorella Irupaylla <fiorella.liz.irupaylla@gmail.com>
2024-09-22 12:44:13 -0500

    123

--- app/src/main/java/dev/fiorellairupaylla/fragmentslab06/ui/adapter/SongAdapter.kt
+++ app/src/main/java/dev/fiorellairupaylla/fragmentslab06/ui/adapter/SongAdapter.kt
@@ -1,4 +1,43 @@
 package dev.fiorellairupaylla.fragmentslab06.ui.adapter
 
-class SongAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import dev.fiorellairupaylla.fragmentslab06.R
+import dev.fiorellairupaylla.fragmentslab06.ui.model.SongModel
+
+class SongAdapter(private var lstSong: List<SongModel>)
+    : RecyclerView.Adapter<SongAdapter.ViewHolder>() {
+
+        class  ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+            val ivSong = itemView.findViewById<ImageView>(R.id.ivSong)
+            val tvSong = itemView.findViewById<TextView>(R.id.tvSong)
+            val tvAlbum = itemView.findViewById<TextView>(R.id.tvAlbum)
+            val tvNumberPlay = itemView.findViewById<TextView>(R.id.tvNumber)
+            val tvDuration = itemView.findViewById<TextView>(R.id.tvDuration)
+
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layaoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layaoutInflater.inflate(R.layout.item_musica, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return lstSong.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val itemSong = lstSong[position]
+        holder.ivSong.setImageResource(itemSong.image)
+        holder.tvSong.text = itemSong.songName
+        holder.tvAlbum.text = itemSong.album
+        holder.tvNumberPlay.text = itemSong.numberPlay
+        holder.tvDuration.text = itemSong.duration
+    }
+
+
 }
--- app/src/main/java/dev/fiorellairupaylla/fragmentslab06/ui/model/SongModel.kt
+++ app/src/main/java/dev/fiorellairupaylla/fragmentslab06/ui/model/SongModel.kt
@@ -1,3 +1,9 @@
 package dev.fiorellairupaylla.fragmentslab06.ui.model
 
-data class SongModel()
+data class SongModel(
+    val id: Int,
+    val image: Int,
+    val songName: String,
+    val album: String,
+    val numberPlay: String,
+    val duration: String)
